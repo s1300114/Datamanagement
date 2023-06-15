cf5a523317b7c30805618a1ebc116b6c5591c9de
s1300114 <s1300114@u-aizu.ac.jp>
2023-06-15 15:08:26 +0900

    Greeting added

--- DiceGame.java
+++ DiceGame.java
@@ -1,9 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class DiceGame{
     public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
 	Random rand = new Random();
 
+	System.out.println("What is your name?");
+	String name = sc.nextLine();
+	System.out.println("Hello, " + name + "!");
+	
 	int dice1 = rand.nextInt(7) + 1;
 	int dice2 = rand.nextInt(7) + 1;
 	int total = dice1 + dice2;
